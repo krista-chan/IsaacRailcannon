6b950b13f75ffeb7cd23571756275b11b9d81182
krista-chan <qbotdev84@gmail.com>
2022-04-13 13:32:52 +0100

    Initial commit

---
+++ IsaacRailcannon/Patcher.cs
@@ -0,0 +1,26 @@
+using HarmonyLib;
+using UnityEngine;
+
+namespace UltraIsaacAudio
+{
+    public static class Patcher
+    {
+        public static void Patch()
+        {
+            var h = new Harmony("com.ultrakill.patch.isaac");
+            h.PatchAll();
+
+            Plugin.Log.LogInfo("Applied harmony patches");
+        }
+    }
+
+    [HarmonyPatch(typeof(Railcannon), "OnEnable")]
+    internal class Patch01
+    {
+        static void Postfix(Railcannon __instance)
+        {
+            AudioClip clip = Plugin.AudioAssetBundle.LoadAsset<AudioClip>("dogma_brimstone.wav"); 
+            __instance.fireSound.GetComponent<AudioSource>().clip = clip;
+        }
+    }
+}
---
+++ IsaacRailcannon/Plugin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+using System.IO;
+
+namespace UltraIsaacAudio
+{
+    [BepInPlugin("com.ultrakill.isaacrailcannon", "IsaacRailcannon", "0.1.0")]
+    [BepInProcess("ULTRAKILL.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private static readonly string BaseDirectory = Directory.GetParent(Application.dataPath).FullName;
+        internal static ManualLogSource Log;
+        internal static readonly AssetBundle AudioAssetBundle = AssetBundle.LoadFromFile(Path.Combine(BaseDirectory, "BepInEx/plugins/IsaacRailcannon/Assets/IsaacRailcannon"));
+
+        private void Awake()
+        {
+            Log = Logger;
+            Log.LogInfo("Loaded plugin");
+        }
+
+        private void Start()
+        {
+            Patcher.Patch();
+        }
+    }
+}
